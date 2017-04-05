6bf3023d9ce5191422267c5aa3ec07877e2c5036
ryan-strawser <r-strawser@onu.edu>
2017-04-04 21:16:28 -0400

    Add files via upload

---
+++ MimicAdHW03/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>
+            Assignment 03
+        </title>
+        <link rel="stylesheet" href="MimicAddCSS.css">
+    </head>
+    <body> 
+        <table>
+            
+                <tr>
+                    <td id="newImg">
+
+                 <span id="divTopImg">LOCAL & LONG-DISTANCE MOVES | COMMERCIAL & RESIDENTIAL | PACKING</span>
+                 <p id="divBottom">GET YOUR FREE QUOTE TODAY!</p>
+
+                    </td>
+               </tr>
+                <tr>
+                    <td id="divTruckMen">
+                        <span id="widen"><p id="p1TruckMen">TWO MEN AND A TRUCK<span id="pdivReg">&reg;</span>
+                        <span id="pdivColumbus">| COLUMBUS</span>  </span>
+                            </p>
+                    <p id="pTruckMen"><span id="pFontNum">614.224.MOVE | TWOMEN.COM<span></p>
+                    </td>
+                </tr>
+                <tr>
+                    <td id="divDodoBird">
+                        
+                    <p id="pfirstBot">Each franchise is independently owned and operated.</p>
+                    <p id="psecondBot">U.S. DOT No. 1146396 | MC 457791 | PUCO #129005-HG</p>
+                    <div id="pdiv1">CD-0006062472-01</div>
+                    
+                    
+                    </td>
+                </tr>
+        </table>
+    </body>
+</html>
