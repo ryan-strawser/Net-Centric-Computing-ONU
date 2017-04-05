279d1029ac57e518b4d30f652315d65f74913063
ryan-strawser <r-strawser@onu.edu>
2017-04-04 21:15:07 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>
+            Assignment 02
+        </title>
+        <link rel="stylesheet" href="FirstImpressionCSS.css">
+    </head>
+    <body> 
+        <table class="ex1" border="0">
+            <tr>
+                <td><p id="inset"><a href="https://www.google.com/">Google</a></inset>
+                </td>
+            </tr>
+        </table><br>
+                <table class="ex2" border="0">
+            <tr>
+                <td><p id="inset"><a href="https://www.amazon.com/">Amazon</a></inset> 
+                </td>
+            </tr>
+        </table><br>
+        </p>
+        <div id="container">
+            <div id="center">
+                Hello, World!
+                </div> 
+             
+        </div>
+    </body>
+</html>
