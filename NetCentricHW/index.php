00eb53c2859ccb5c2678a9226737217457cef7fb
ryan-strawser <r-strawser@onu.edu>
2017-04-04 21:14:35 -0400

    Add files via upload

---
+++ NetCentricHW/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>
+            Assignment 02
+        </title>
+        <link rel="stylesheet" href="FirstImpression.css">
+    </head>
+    <body id = "body"> 
+        <table id="ex1" id="1">
+            <tr>
+                <td><p id="inset"><a href="https://www.google.com/">Google</a> <a href="https://www.amazon.com/">Amazon</a></p>
+                </td>
+            </tr>
+        </table><br>
+        <div id="container">
+            <div id="center">
+                Hello, World!
+                </div> 
+             
+        </div>
+    </body>
+</html>
